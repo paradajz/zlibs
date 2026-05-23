bd9a35e7769e3926790c4051c7329eea30497e11
paradajz <2544094+paradajz@users.noreply.github.com>
2026-05-23 08:46:49 +0000

    refactor: qualify MIDI transport namespaces
    on some targets the names could clash with built in types

--- module/src/utils/midi/transport/transport_serial.cpp
+++ module/src/utils/midi/transport/transport_serial.cpp
@@ -6,44 +6,45 @@
 
 #include "zlibs/utils/midi/transport/serial/transport_serial.h"
 
-using namespace zlibs::utils::midi::serial;
-
-bool Serial::Transport::supported()
+namespace zlibs::utils::midi::serial
 {
-    return _serial._hwa.supported();
-}
+    bool Serial::Transport::supported()
+    {
+        return _serial._hwa.supported();
+    }
 
-bool Serial::Transport::init()
-{
-    return _serial._hwa.init();
-}
+    bool Serial::Transport::init()
+    {
+        return _serial._hwa.init();
+    }
 
-bool Serial::Transport::deinit()
-{
-    return _serial._hwa.deinit();
-}
+    bool Serial::Transport::deinit()
+    {
+        return _serial._hwa.deinit();
+    }
 
-bool Serial::Transport::write_midi_byte(uint8_t data)
-{
-    return _serial._hwa.write(data);
-}
+    bool Serial::Transport::write_midi_byte(uint8_t data)
+    {
+        return _serial._hwa.write(data);
+    }
 
-bool Serial::Transport::write(const midi_ump& packet)
-{
-    return midi::write_ump_as_midi1_bytes(packet,
-                                          UMP_GROUP(packet),
-                                          [this](uint8_t data)
-                                          {
-                                              return write_midi_byte(data);
-                                          });
-}
+    bool Serial::Transport::write(const midi_ump& packet)
+    {
+        return midi::write_ump_as_midi1_bytes(packet,
+                                              UMP_GROUP(packet),
+                                              [this](uint8_t data)
+                                              {
+                                                  return write_midi_byte(data);
+                                              });
+    }
 
-std::optional<midi_ump> Serial::Transport::read()
-{
-    return midi::read_midi1_bytes_as_ump(_parser,
-                                         DEFAULT_RX_GROUP,
-                                         [this]()
-                                         {
-                                             return _serial._hwa.read();
-                                         });
-}
+    std::optional<midi_ump> Serial::Transport::read()
+    {
+        return midi::read_midi1_bytes_as_ump(_parser,
+                                             DEFAULT_RX_GROUP,
+                                             [this]()
+                                             {
+                                                 return _serial._hwa.read();
+                                             });
+    }
+}    // namespace zlibs::utils::midi::serial
